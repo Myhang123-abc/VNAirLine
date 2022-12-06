70ff8b732224c1bbbb3935ded8d6e0e4da36ede1
Anh Tuấn <tuannguyen8531@gmail.com>
2022-12-06 10:42:03 +0700

    Thêm lệnh xuất random

--- VNAirLine/VNAirLine.cpp
+++ VNAirLine/VNAirLine.cpp
@@ -4,6 +4,9 @@
 int main()
 {
     srand(time(NULL));
+    RandomGenerator random;
+    string a = random.randomFlightNumGen(4,3);
+    cout << a;
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
