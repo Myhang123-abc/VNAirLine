0658914ec9667bbdbbbcf0587d731f16007bf981
Anh Tuấn <tuannguyen8531@gmail.com>
2022-12-09 10:33:40 +0700

    Sửa

--- VNAirLine/FlightDistance.cpp
+++ VNAirLine/FlightDistance.cpp
@@ -2,8 +2,8 @@
 void displayMeasurementInstructions()
 {
     string symbols = "+---------------------------+";
-    printf("\n\n %100s\n %100s", symbols, "| SOME IMPORTANT GUIDELINES |","");
-    printf("\n %100s\n", symbols,"");
+    printf("\n\n %100s\n %100s", symbols, "| SOME IMPORTANT GUIDELINES |");
+    printf("\n %100s\n", symbols);
     printf("\n\t\t1. Distance between the destinations are based upon the Airports Coordinates(Latitudes && Longitudes) based in those cities\n");
     printf("\t\t2. Actual Distance of the flight may vary from this approximation as Airlines may define their on Travel Policy that may restrict the planes to fly through specific regions...\n");
     printf("\t\t3. Flight Time depends upon several factors such as Ground Speed(GS), AirCraft Design, Flight Altitude and Weather. Ground Speed for these calculations is 450 Knots...\n");
