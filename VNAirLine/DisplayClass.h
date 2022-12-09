62705cc4af17df9d98e0380639726bda9cb36e24
Anh Tuấn <tuannguyen8531@gmail.com>
2022-12-09 10:28:49 +0700

    Thêm class.

---
+++ VNAirLine/DisplayClass.h
@@ -0,0 +1,16 @@
+#pragma once
+#ifndef DisplayClass_HEADER
+#define DisplayClass_HEADER
+#include "Header.h"
+#include "Flight.h"
+#include "Customer.h"
+class DisplayClass
+{
+	virtual void displayRegisteredUsersForAllFlight() = 0;
+	virtual void displayRegisteredUsersForASpecificFlight(string flightNum) = 0;
+	virtual void displayHeaderForUsers(Flight flight, vector<Customer> c) = 0;
+	virtual void displayFlightsRegisteredByOneUser(string userID) = 0;
+	virtual void displayArtWork(int options) = 0;
+};
+#endif // !DisplayClass_HEADER
+
