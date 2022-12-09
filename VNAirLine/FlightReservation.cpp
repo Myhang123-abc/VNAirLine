9015037c9f7135fcbda98730e18eee32cb26966c
Anh Tuấn <tuannguyen8531@gmail.com>
2022-12-09 16:18:16 +0700

    Thêm class FlightReservation

---
+++ VNAirLine/FlightReservation.cpp
@@ -0,0 +1,1 @@
+#include "FlightReservation.h"
---
+++ VNAirLine/FlightReservation.h
@@ -0,0 +1,12 @@
+#pragma once
+#ifndef FlightReservation_HEADER
+#define FlightReservation_HEADER
+#include "Header.h"
+#include "DisplayClass.h"
+class FlightReservation
+{
+
+};
+#endif // !FlightReservation_HEADER
+
+
